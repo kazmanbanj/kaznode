45f9f999dfe386a6fab57bb6307a87418a71c99b
Kazeem Banjoko <banjoko10@gmail.com>
2022-07-14 08:57:08 +0100

    installed and implemented pug templating engine for the view

--- routes/admin.js
+++ routes/admin.js
@@ -4,6 +4,8 @@ const rootDir = require('../utils/path')
 
 const router = express.Router();
 
+const products = [];
+
 // /admin/add-product - GET
 router.get('/add-product', (req, res, next) => {
     res.sendFile(path.join(rootDir, 'resources', 'views', 'add-product.html'));
@@ -11,8 +13,10 @@ router.get('/add-product', (req, res, next) => {
 
 // /admin/add-product - POST
 router.post('/add-product', (req, res, next) => {
-    console.log(req.body);
+    products.push({ title: req.title.body });
     res.redirect('/');
 });
 
-module.exports = router;
+// module.exports = router;
+exports.routes = router;
+exports.products = products;
--- routes/shop.js
+++ routes/shop.js
@@ -1,11 +1,13 @@
 const path = require('path');
 const express = require('express');
-const rootDir = require('../utils/path')
+const rootDir = require('../utils/path');
+const adminData = require('./admin');
 
 const router = express.Router();
 
 router.get('/', (req, res, next) => {
-    res.sendFile(path.join(rootDir, 'resources', 'views', 'shop.html'));
+    // res.sendFile(path.join(rootDir, 'resources', 'views', 'shop.html'));
+    res.render('pug', 'shop')
 });
 
 module.exports = router;
