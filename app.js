c911b35ffd88c60562572b3589a1e7a5a49cd935
Kazeem Banjoko <banjoko10@gmail.com>
2022-07-16 07:30:20 +0100

    converted html files to pug

--- app.js
+++ app.js
@@ -17,7 +17,8 @@ app.use('/admin', adminData.routes);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
-    res.status(404).sendFile(path.join(__dirname, 'resources', 'views', '404.html'));;
+    // res.status(404).sendFile(path.join(__dirname, 'resources', 'views', '404.html'));;
+    res.render('pug/404', {docTitle: 'Page not found'});
 });
 
 app.listen(3000)
