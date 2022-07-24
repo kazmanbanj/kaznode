6cc086f0d9dae30cb467e1695950456a0d058bd8
Kazeem Banjoko <banjoko10@gmail.com>
2022-07-24 15:48:34 +0100

    displaing proudct data

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # kaznode
+
+<!-- https://media.istockphoto.com/photos/string-light-bulbs-at-sunset-picture-id1300384615?b=1&k=20&m=1300384615&s=170667a&w=0&h=rkDm5TdJp_dU7VAknk4EuZEZ2ho2QQspOavjlwGrsuI= -->
--- controllers/admin.js
+++ controllers/admin.js
@@ -8,7 +8,12 @@ exports.getAddProducts = (req, res, next) => {
 };
 
 exports.postAddProducts = (req, res, next) => {
-    const product = new Product(req.body.title);
+    const title = req.body.title;
+    const imageUrl = req.body.imageUrl;
+    const price = req.body.price;
+    const description = req.body.description;
+    const product = new Product(title, imageUrl, price, description);
+    console.log(product);
     product.save();
 
     res.redirect('/');
