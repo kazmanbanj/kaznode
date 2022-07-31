6eb2494b060dee0e0f60fc6ad2d1395f60f45c80
Kazeem Banjoko <banjoko10@gmail.com>
2022-07-31 22:31:18 +0100

    Pre-populating the edit page with edit data

--- models/cart.js
+++ models/cart.js
@@ -32,7 +32,7 @@ module.exports = class Cart {
             cart.totalPrice = cart.totalPrice + +productPrice;
             fs.writeFile(p, JSON.stringify(cart), err => {
                 console.log(err);
-            })
+            });
         })
     }
 }
--- routes/admin.js
+++ routes/admin.js
@@ -7,9 +7,10 @@ const router = express.Router();
 // /admin/add-product -> GET
 router.get('/add-product', adminController.getAddProducts);
 router.get('/products', adminController.getProducts);
-router.get('/edit-product', adminController.getEditProduct);
+router.get('/edit-product/:productId', adminController.getEditProduct);
 
 // /admin/add-product -> POST
-router.post('/add-product', adminController.postAddProducts);
+router.post('/add-product', adminController.postEditProduct);
+router.post('/edit-product', adminController.getEditProduct);
 
 module.exports = router;
