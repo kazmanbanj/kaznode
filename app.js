ce4c15d9ef7c5c3746d2b7b7db59d506278aa62f
Kazeem Banjoko <banjoko10@gmail.com>
2022-08-04 20:44:21 +0100

    adding a user model

--- app.js
+++ app.js
@@ -20,6 +20,16 @@ const shopRoutes = require('./routes/shop');
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(express.static(path.join(__dirname, 'public')));
 
+// retrieving a user
+app.use((req, res, next) => {
+    User.findByPk(1)
+        .then(user => {
+            req.user = user;
+            next();
+        })
+        .catch(err => console.log(err));
+})
+
 // app.use('/admin', adminData.routes);
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
@@ -29,11 +39,20 @@ app.use(errorController.get404);
 Product.belongsTo(User, { constraints: true, onDelete: 'CASCADE' });
 User.hasMany(Product);
 
-sequelize.sync({force: true})
-    .then((result) => {
-        // console.log(result);
+sequelize
+    // .sync({force: true})
+    .sync()
+    .then(result => {
+        return User.findByPk(1);
+    })
+    .then(user => {
+        if (!user) {
+            return User.create({name: 'John Doe', email: 'johndoe@email.com'});
+        }
+        return user;
+    })
+    .then(user => {
+        console.log(user);
         app.listen(3000);
     })
-    .catch((err) => {
-        console.log(err);
-    });
+    .catch(err => console.log(err));
