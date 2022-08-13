cba0b1fc409e65a977d1c12ef2c43f2d7c6fc3b0
Kazeem Banjoko <banjoko10@gmail.com>
2022-08-13 17:08:36 +0100

    Resetting the Cart & Fetching and Outputting Orders

--- controllers/shop.js
+++ controllers/shop.js
@@ -104,16 +104,11 @@ exports.postCartDeleteProduct = (req, res, next) => {
     .catch(err => console.log(err));
 }
 
-exports.getCheckout = (req, res, next) => {
-    res.render('ejs/shop/checkout', {
-        docTitle: 'Checkout',
-        path: '/checkout'
-    })
-}
-
 exports.postOrder = (req, res, next) => {
+    let fetchedCart;
     req.user.getCart()
     .then(cart => {
+        fetchedCart = cart;
         return cart.getProducts();
     })
     .then(products => {
@@ -129,14 +124,29 @@ exports.postOrder = (req, res, next) => {
         .catch(err => console.log(err));
     })
     .then(result => {
+        return fetchedCart.setProducts(null);
+    })
+    .then(result => {
         res.redirect('/orders');
     })
     .catch(err => console.log(err));
 }
 
 exports.getOrders = (req, res, next) => {
-    res.render('ejs/shop/orders', {
-        docTitle: 'Your Orders',
-        path: '/orders'
+    req.user.getOrders({include: ['products']})
+    .then(orders => {
+        res.render('ejs/shop/orders', {
+            docTitle: 'Your Orders',
+            path: '/orders',
+            orders: orders
+        });
     })
+    .catch(err => console.log(err));
 }
+
+// exports.getCheckout = (req, res, next) => {
+//     res.render('ejs/shop/checkout', {
+//         docTitle: 'Checkout',
+//         path: '/checkout'
+//     })
+// }
